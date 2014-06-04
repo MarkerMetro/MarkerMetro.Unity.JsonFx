ce75e86ebc1d13e27cc28beb1d58688f4df15ead
Keith Patton <keith.p@markermetro.com>
2014-06-04 20:38:09 +1200

    updated winlegacy dependency to latest and also removed unnecessary files

--- JsonFx.Tests/CollectionTest.cs
+++ JsonFx.Tests/CollectionTest.cs
@@ -20,7 +20,6 @@ namespace JsonFxMetro.Tests
     public class CollectionTest
     {
 
-
         [TestMethod]
 #if NETFX_CORE
         public void MetroCollectionTest_sample()
@@ -29,33 +28,12 @@ namespace JsonFxMetro.Tests
 #else
         public void CollectionTest_sample()
 #endif
-        {
-            //JsonReaderSettings readerSettings = new JsonReaderSettings();
-            //readerSettings.TypeHintName = StronglyTyped.MyTypeHintName;
-            //readerSettings.AllowNullValueTypes = true;
-            //readerSettings.AllowUnquotedObjectKeys = false;
-
-            //string source = @"{""data"":[{""file"":""full"",""textureId"":""full"",""type"":""tshirt_pattern""},{""file"":""strip_diagonal"",""textureId"":""strip_diagonal"",""type"":""tshirt_pattern""}]}";
-
-            string source = @"{""file"":""full"",""textureId"":""full"",""type"":""tshirt_pattern"", ""data"":[{""file"":""full"",""textureId"":""full"",""type"":""tshirt_pattern""},{""file"":""strip_diagonal"",""textureId"":""strip_diagonal"",""type"":""tshirt_pattern""}]}";
-
-            //object obj = null;
-            //new JsonReader(value)).Deserialize(start, type)
-            //var reader  = new JsonReader(source);
-            //var test = reader.Deserialize(0, typeof(TextureFilesCatalog));
-
+        { 
+            string source = @"{""data"":[{""file"":""full"",""textureId"":""full"",""type"":""tshirt_pattern""},{""file"":""strip_diagonal"",""textureId"":""strip_diagonal"",""type"":""tshirt_pattern""}]}";
             var output = JsonReader.Deserialize<TextureFilesCatalog>(source);
-            //var output = JsonReader.Deserialize<SimpleValueObject>(source);
-
-
             Assert.IsNotNull(output);
             Assert.IsNotNull(output.textureFiles);
             Assert.IsTrue(output.textureFiles.Count > 0);
-            
-            //Deserialize((obj == null) ? null : obj.GetType());
-            //writer.WriteLine("READ: " + unitTestFile);
-            //writer.WriteLine("Result: {0}", (obj == null) ? "null" : obj.GetType().FullName);
-
         }
 
     }
