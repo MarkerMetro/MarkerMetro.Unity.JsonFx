d8de175aff7d0ebb3ef252154c395db89c1cfb87
Keith Patton <keith.p@markermetro.com>
2014-06-04 17:30:39 +1200

    more test stuff

---
+++ JsonFxMetro.Tests/Types/SimpleValueObject.cs
@@ -0,0 +1,12 @@
+﻿using Pathfinding.Serialization.JsonFx;
+using System.Collections.Generic;
+public class SimpleValueObject
+{
+
+    public string file;
+    public string textureId;
+    public string type;
+
+    [JsonName("data")]
+    public List<SimpleValueObject> SimpleValueObjects { get; set; }
+}
